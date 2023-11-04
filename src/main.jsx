cf2aefe215f2ddcc3984ee44f310a2ee15799b38
Rafaelx <meow7747@gmail.com>
2023-11-04 18:44:01 +0700

    Set up Route and Environment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import SalesReport from './pages/SalesReport.jsx'
+import Customer from './pages/Customer.jsx'
+import Inventory from './pages/Inventory.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App /> 
+  }, 
+  {
+    path: '/sales-report',
+    element: <SalesReport /> 
+  },
+  {
+    path: '/inventory',
+    element: <Inventory />
+  },
+  {
+    path: '/customer',
+    element: <Customer />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router}/>
 )
